adc7ecef2233ecc938d1144e38d99dec777a2c0e
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2023-11-07 14:18:14 +0530

    add para generator

---
+++ paragraph generator/script.js
@@ -0,0 +1,93 @@
+const para = [
+  `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+Totam consectetur veritatis magnam vero odit maxime dicta 
+consequatur numquam error omnis voluptatum, mollitia doloremque 
+inventore. Vitae distinctio temporibus maxime perspiciatis nisi!
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores,
+ praesentium! `,
+  `Iure a rem officia totam accusamus quis repellat cumque
+ hic eligendi blanditiis ea perspiciatis asperiores quidem soluta tempore
+  suscipit, porro sunt! Debitis in beatae vero suscipit unde adipisci 
+  magni animi harum vel. Obcaecati, odio? Vero iste obcaecati assumenda 
+  iusto consequuntur eius voluptas. Quod iste pariatur quo facilis consequuntur,
+   mollitia quaerat non minus magnam, nisi quae inventore quidem.`,
+  `Iure a rem officia totam accusamus quis repellat cumque
+ hic eligendi blanditiis ea perspiciatis asperiores quidem soluta tempore
+  suscipit, porro sunt! Debitis in beatae vero suscipit unde adipisci 
+  magni animi harum vel. Obcaecati, odio? Vero iste obcaecati assumenda 
+  iusto consequuntur eius voluptas. Quod iste pariatur quo facilis consequuntur,
+   mollitia quaerat non minus magnam, nisi quae inventore quidem.`,
+  `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+Totam consectetur veritatis magnam vero odit maxime dicta 
+consequatur numquam error omnis voluptatum, mollitia doloremque 
+inventore. Vitae distinctio temporibus maxime perspiciatis nisi!
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores,
+ praesentium! `,
+  `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+Totam consectetur veritatis magnam vero odit maxime dicta 
+consequatur numquam error omnis voluptatum, mollitia doloremque 
+inventore. Vitae distinctio temporibus maxime perspiciatis nisi!
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores,
+ praesentium! `,
+  `Iure a rem officia totam accusamus quis repellat cumque
+ hic eligendi blanditiis ea perspiciatis asperiores quidem soluta tempore
+  suscipit, porro sunt! Debitis in beatae vero suscipit unde adipisci 
+  magni animi harum vel. Obcaecati, odio? Vero iste obcaecati assumenda 
+  iusto consequuntur eius voluptas. Quod iste pariatur quo facilis consequuntur,
+   mollitia quaerat non minus magnam, nisi quae inventore quidem.`,
+  `Iure a rem officia totam accusamus quis repellat cumque
+ hic eligendi blanditiis ea perspiciatis asperiores quidem soluta tempore
+  suscipit, porro sunt! Debitis in beatae vero suscipit unde adipisci 
+  magni animi harum vel. Obcaecati, odio? Vero iste obcaecati assumenda 
+  iusto consequuntur eius voluptas. Quod iste pariatur quo facilis consequuntur,
+   mollitia quaerat non minus magnam, nisi quae inventore quidem.`,
+  `Iure a rem officia totam accusamus quis repellat cumque
+ hic eligendi blanditiis ea perspiciatis asperiores quidem soluta tempore
+  suscipit, porro sunt! Debitis in beatae vero suscipit unde adipisci 
+  magni animi harum vel. Obcaecati, odio? Vero iste obcaecati assumenda 
+  iusto consequuntur eius voluptas. Quod iste pariatur quo facilis consequuntur,
+   mollitia quaerat non minus magnam, nisi quae inventore quidem.`,
+  `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+Totam consectetur veritatis magnam vero odit maxime dicta 
+consequatur numquam error omnis voluptatum, mollitia doloremque 
+inventore. Vitae distinctio temporibus maxime perspiciatis nisi!
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores,
+ praesentium! `,
+  `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+Totam consectetur veritatis magnam vero odit maxime dicta 
+consequatur numquam error omnis voluptatum, mollitia doloremque 
+inventore. Vitae distinctio temporibus maxime perspiciatis nisi!
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores,
+ praesentium! `,
+  `Iure a rem officia totam accusamus quis repellat cumque
+ hic eligendi blanditiis ea perspiciatis asperiores quidem soluta tempore
+  suscipit, porro sunt! Debitis in beatae vero suscipit unde adipisci 
+  magni animi harum vel. Obcaecati, odio? Vero iste obcaecati assumenda 
+  iusto consequuntur eius voluptas. Quod iste pariatur quo facilis consequuntur,
+   mollitia quaerat non minus magnam, nisi quae inventore quidem.`,
+];
+
+const loremForm = document.querySelector(".lorem-form");
+const input = document.querySelector("#amount");
+const result = document.querySelector(".lorem-text");
+
+loremForm.addEventListener("click", function (e) {
+  const value = input.value
+  const random = Math.floor(Math.random() * para.length);
+
+  if (isNaN(value) || value <= 0 || value > para.length) {
+    result.innerHTML = `<p>${para[random]}<p>`;
+  } else {
+    let newpara = para.slice(0, value);
+
+    let x = newpara
+      .map(function (item) {
+        return (result.innerHTML = `<p>${item}</p>`);
+      })
+      .join("");
+
+    result.innerHTML = x;
+  }
+
+  console.log(random);
+});
